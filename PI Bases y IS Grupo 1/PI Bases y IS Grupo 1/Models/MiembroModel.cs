45d9e1fe4d24fe775590fbba50a3558c17864452
hellenfdz12 <hellenfdz12@gmail.com>
2020-09-12 23:17:40 -0600

    H2: Aun no logramos conectar modelo con el controlador., al menos no lo hemos lograd ver. Pair programming

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/MiembroModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //Para las librerias de Required y Display. Para hacer binding de las propiedades del modelo vista
+using System.Web; //Para incluir los objetos HttpPostedFileBase para recuperar archivos (pdf,imagenes)
+using System.ComponentModel;
+
+namespace PIBasesISGrupo1.Models
+{
+    public class MiembroModel
+    {
+        public int Id { get; set; }
+        public string Genero { get; set; }
+        [Required(ErrorMessage = "Es necesario que ingreses tu nombre")]
+        [Display(Name = "Ingrese su Nombre")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "Es necesario que ingreses tu primer apellido")]
+        [Display(Name = "Ingrese su primer apellido")]
+        public string PrimerApellido{ get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingreses tu segundo apellido")]
+        [Display(Name = "Ingrese su segundo apellido")]
+        public string SegundoApellido { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingreses un correo")]
+        [Display(Name = "Ingrese su correo")]
+        //[RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar numeros")]
+        public string Correo { get; set; }        
+        public string Contraseña { get; set; }
+        public string Nacionalidad { get; set; }
+    }
+}
--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembro/Miembro.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembro/Miembro.cshtml.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using PIBasesISGrupo1.Models;
-namespace PI_Bases_y_IS_Grupo_1.Pages
-{
-    public class IndexModel : PageModel
+
+namespace PIBasesISGrupo_1.Pages.Miembro
+{s
+    public class MiembroModel : PageModel
     {
-        
         public void OnGet()
+        {
+
+        }
+
+        public void OnPost(int id)
         {
 
         }
