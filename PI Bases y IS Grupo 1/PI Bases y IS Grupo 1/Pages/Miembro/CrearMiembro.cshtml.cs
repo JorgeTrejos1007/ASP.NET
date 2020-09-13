532be33e15c51a56e491f4b96a108d84af148d93
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-09-13 08:57:24 -0600

    H2:Se corrigen algunos nombres de vistas

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembro/CrearMiembro.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembro/CrearMiembro.cshtml.cs
@@ -4,19 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PIBasesISGrupo1.Models;
 
-namespace PIBasesISGrupo_1.Pages.Miembro
+namespace PIBasesISGrupo1.Pages.Miembro
 {
-    public class MiembroModel : PageModel
+    public class CrearMiembroModel : PageModel
     {
+
+        public MiembroModel Miembro { get; set; }
         public void OnGet()
         {
 
         }
-
         public void OnPost(int id)
         {
-           
+
         }
+        public void OnPostMiembro() {
+        } 
     }
 }
