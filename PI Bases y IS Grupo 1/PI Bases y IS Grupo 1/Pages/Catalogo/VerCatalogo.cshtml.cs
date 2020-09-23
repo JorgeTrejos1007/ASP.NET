31196bf8c9fd223c44ccb353bf97497c8b7f888a
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-09-23 10:22:44 -0600

    Catalogo muestra los topicos y sus categorias

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Catalogo/VerCatalogo.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PIBasesISGrupo1.Models;
+using PIBasesISGrupo1.Handler;
+
+namespace PIBasesISGrupo1.Pages
+{
+    public class CatalogoModel : PageModel
+    {
+        [BindProperty]
+        public string[] topicosAelegir { get; set; }
+
+        public void OnGet()
+        {
+            CatalogoHandler accesoCatalago = new CatalogoHandler();
+            ViewData["TopicosYCategorias"] = accesoCatalago.obteneTodosLosTopicosYCategoriasAsociadas();
+        }
+
+
+        public void OnPost()
+        {
+            CatalogoHandler accesoCatalago = new CatalogoHandler();
+            
+        }
+
+    }
+}
