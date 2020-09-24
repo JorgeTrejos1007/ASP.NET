d591df697f578cdd22dc414646eb7f2f31981b7d
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-09-24 17:07:44 -0600

    Se crea el OnGet en la vista

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/AprobarCurso.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/AprobarCurso.cshtml.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.Net.Mime;
+using System.IO;
+using PIBasesISGrupo1.Handler;
+using PIBasesISGrupo1.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 namespace PIBasesISGrupo1.Pages.Curso
 {
     public class AprobarCursoModel : PageModel
     {
         public void OnGet()
         {
-
+            CursoHandler cursoHandler = new CursoHandler();
+            ViewData["CursosPropuestos"] = cursoHandler.obtenerCursosPropuestos();
+        }
+        public void onPost()
+        {
         }
     }
 }
