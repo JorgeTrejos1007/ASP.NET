1bd12c3dc8f7be422850192e725b1e77437c34d6
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-09-24 09:58:29 -0600

    El boton de agregar categoria ya es funcional

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Catalogo/EditarCatalogo.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Catalogo/EditarCatalogo.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PIBasesISGrupo1.Models;
+using PIBasesISGrupo1.Handler;
 
 namespace PIBasesISGrupo1.Pages.Catalogo
 {
     public class EditarCatalogoModel : PageModel
     {
+        [BindProperty]
+        string categoria { get; set; }
+
         public void OnGet()
         {
 
         }
+
+
+        public void OnPostAgregarCategoria()
+        {
+            CatalogoHandler accesoCatalogo = new CatalogoHandler();
+            accesoCatalogo.insertarCategoria(categoria);
+
+        }
+
     }
 }
