e59bcd826971860e393b0a59de9eab490b277965
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-09-24 09:50:11 -0600

    Nuevo diseño de catalogo

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Catalogo/EditarCatalogo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIBasesISGrupo1.Pages.Catalogo
+{
+    public class EditarCatalogoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
