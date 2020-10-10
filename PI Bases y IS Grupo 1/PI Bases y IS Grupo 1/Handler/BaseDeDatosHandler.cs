8b67b8c2d7230bc582a44cd3dda69c5aa77a0b57
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-10-10 09:20:48 -0600

    Se ha iniciado el handler que se va a encargar de comunicarse con la base de datos

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/BaseDeDatosHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Web;
+
+namespace PIBasesISGrupo1.Handler
+{
+
+    public class BaseDeDatosHandler
+    {
+        private ConexionModel conexionBD;
+        private SqlConnection conexion;
+
+        public BaseDeDatosHandler()
+        {
+            conexionBD = new ConexionModel();
+            conexion = conexionBD.Connection();
+        }
+
+        public DataTable crearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+
+    }
+}
