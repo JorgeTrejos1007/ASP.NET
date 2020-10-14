6931ea7f854aa57f47efea269a1d4af2927ad21c
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-10-14 12:23:05 -0600

    Se realizan cambios en la vista y controlador de inscribirme a un curso

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/InscribirmeCurso.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/InscribirmeCurso.cshtml.cs
@@ -12,12 +12,38 @@ namespace PIBasesISGrupo1.Pages.Curso
     {
         [BindProperty]
         public Miembro miembro { get; set; }
-
         [BindProperty]
         public IFormFile archivoImagen { get; set; }
+        public string[] idiomas = new string[73]
+                  { "Azeri", "Afrikaans", "Albanes", "Aleman", "Alsaciano",
+                        "Anglosajon", "Arabe", "Aragones", "Armenio", "Asturiano", "Aymara", "Bengali", "Bielorruso",
+                        "Birmano", "Bosnio", "Breton","Bulgaro", "Canares", "Catalan", "Chamorro", "Checo", "Cheroqui",
+                        "Chino", "Coreano", "Corso", "Croata", "Curdo", "Danes", "Eslovaco", "Esloveno", "Español", "Esperanto",
+                        "Estonio","Euskera", "Feroes", "Fiyiano", "Finlandes", "Frances", "Frison", "Gales", "Gallego", "Georgiano",
+                        "Griego", "Guarani", "Gujarati","Hebreo", "Hindi", "Holandes", "Hungaro", "Ido","Indonesio", "Ingles",
+                        "Irlandes", "Islandes", "Italiano", "Japones", "Javanes","Latin", "Lituano", "Luxemburgues", "Macedonio",
+                        "Noruego", "Occitano", "Papiamento", "PersaPolaco", "Portugues", "Rumano","Ruso",
+                        "Serbio", "Somali", "Sueco", "Tailandes", "Turco"
+                  };
+        public string[] habilidades = new string[17]
+               {
+                    "Empatia","Saber escuchar","Liderazgo", "Flexibilidad","Optimismo", "Confianza","Honestidad",
+                    "Paciencia","Comunicacion","Convencimiento","Esfuerzo", "Esfuerzo","Dedicacion","Comprension",
+                    "Comprension", "Asertividad", "Credibilidad"
+
+               };
 
         public void OnGet()
         {
+            try
+            {
+                ViewData["idiomas"] = idiomas;
+                ViewData["habilidades"] = habilidades;
+            }
+            catch
+            {
+                Redirect("~/Curso/CrearMiembro");
+            }
 
         }
 
