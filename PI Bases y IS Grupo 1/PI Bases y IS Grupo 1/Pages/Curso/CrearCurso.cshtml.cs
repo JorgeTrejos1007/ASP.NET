a97b9a7dfd6a026314654ae8d723e78c3da0f507
AdrianRojas <rojas3099@gmail.com>
2020-10-14 13:10:51 -0600

    agregar multimples materiales

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/CrearCurso.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/CrearCurso.cshtml.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using PIBasesISGrupo1.Handler;
+using PIBasesISGrupo1.Models;
 namespace PIBasesISGrupo1.Pages.Curso
 {
     public class CrearCursoModel : PageModel
