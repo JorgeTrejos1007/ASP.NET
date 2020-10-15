0c48caa5da1308a927334b68c655b5e2a8514ec2
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-10-15 17:59:05 -0600

    Se ha implementado la logica para crear un archivo de excel

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Encuestas/MostrarRespuestas.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Encuestas/MostrarRespuestas.cshtml.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PIBasesISGrupo1.Models;
 using PIBasesISGrupo1.Handler;
-
+using System.Text;
+using ClosedXML.Excel;
+using System.IO;
 
 namespace PIBasesISGrupo1.Pages.Encuestas
 {
@@ -44,13 +46,25 @@ namespace PIBasesISGrupo1.Pages.Encuestas
 
 
 
-        public void OnPostExportarRespuestas(int id)
+        public IActionResult OnPostExportarRespuestas(int id)
         {
-            
+            var registroDeRespuestas = new XLWorkbook();
+            var hojaDeRespuestas = registroDeRespuestas.Worksheets.Add("Respuestas");
+            //fila//columna
+            int fila = 1;
+            hojaDeRespuestas.Cell(fila, 1).Value = "ID";
+            hojaDeRespuestas.Cell(fila, 2).Value = "Nombre";
 
+            fila = fila+1;
+            hojaDeRespuestas.Cell(fila, 1).Value = "0";
+            hojaDeRespuestas.Cell(fila, 2).Value = "Ronny";
 
+            var stream = new MemoryStream();
+            registroDeRespuestas.SaveAs(stream);
+            var content = stream.ToArray();
+            string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
-
+            return File(content, excelContentType,"Respuestas.xlsx");
         }
 
 
