927d13af21e68b45ee1ad8b2ad23f4974ab78c6c
Carlos Cartín Cascante <ala53@hotmail.com>
2020-10-23 19:23:42 -0600

    Cambios realizados a la vista de modificar información del curso, faltan cosas, del handler, y falta cambiar la parte visual

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/CursoHandler.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/CursoHandler.cs
@@ -34,7 +34,8 @@ namespace PIBasesISGrupo1.Handler
             return bytes;
         }
 
-        public bool proponerCurso(Cursos curso, IFormFile archivo) {
+        public bool proponerCurso(Cursos curso, IFormFile archivo)
+        {
             string consulta = "INSERT INTO Curso(nombre,emailEducadorFK,documentoInformativo,tipoDocumentoInformativo,precio)"
           + "VALUES (@nombreCurso,@emailEducador,@documentoInformativo,@tipoDocumentoInformativo,@precio)";
             SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
@@ -48,16 +49,18 @@ namespace PIBasesISGrupo1.Handler
             bool exito = comandoParaConsulta.ExecuteNonQuery() >= 1;
             if (curso.topicos.Length > 0)
             {
-               exito= insertarRelacionConTopico(curso); 
+                exito = insertarRelacionConTopico(curso);
             }
             conexion.Close();
             return exito;
         }
-        public bool insertarRelacionConTopico(Cursos curso) {
+        public bool insertarRelacionConTopico(Cursos curso)
+        {
             bool exito = false;
             string consultaATablaContiene = "INSERT INTO Contiene(nombreCursoFK,nombreTopicoFK)"
             + "VALUES (@nombreCurso,@nombreTopico)";
-            for (int topico = 0; topico < curso.topicos.Length; ++topico) {
+            for (int topico = 0; topico < curso.topicos.Length; ++topico)
+            {
                 SqlCommand comandoParaConsulta = new SqlCommand(consultaATablaContiene, conexion);
                 SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
                 comandoParaConsulta.Parameters.AddWithValue("@nombreCurso", curso.nombre);
@@ -67,7 +70,7 @@ namespace PIBasesISGrupo1.Handler
             return exito;
 
         }
-        public List<Tuple<Cursos,Miembro>> obtenerCursosPropuestos()
+        public List<Tuple<Cursos, Miembro>> obtenerCursosPropuestos()
         {
             List<Tuple<Cursos, Miembro>> cursos = new List<Tuple<Cursos, Miembro>>();
             string consultaCategorias = "SELECT C.nombre AS nombreCurso,C.estado AS estado,C.precio AS precio," +
@@ -78,30 +81,30 @@ namespace PIBasesISGrupo1.Handler
             Miembro educadorTemporal;
             foreach (DataRow columna in tablaCurso.Rows)
             {
-                cursoTemporal= new Cursos
+                cursoTemporal = new Cursos
                 {
                     nombre = Convert.ToString(columna["nombreCurso"]),
                     estado = Convert.ToString(columna["estado"]),
-                    precio=Convert.ToDouble(columna["precio"]),
+                    precio = Convert.ToDouble(columna["precio"]),
                     emailDelEducador = Convert.ToString(columna["emailEducador"]),
-                    byteArrayDocument = (byte [])columna["documento"],
+                    byteArrayDocument = (byte[])columna["documento"],
                     tipoDocInformativo = Convert.ToString(columna["tipoDocumento"])
                 };
                 educadorTemporal = new Miembro
                 {
-                    nombre = Convert.ToString(columna["nombreEducador"]), 
+                    nombre = Convert.ToString(columna["nombreEducador"]),
                     primerApellido = Convert.ToString(columna["primerApellido"]),
                     segundoApellido = Convert.ToString(columna["segundoApellido"])
 
                 };
                 cursos.Add(new Tuple<Cursos, Miembro>(cursoTemporal, educadorTemporal));
 
-            
+
             }
             return cursos;
         }
 
-        public List<Tuple<Cursos, Miembro,List<Tuple<string,string>>>> obtenerCursosDisponibles()
+        public List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> obtenerCursosDisponibles()
         {
             List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> cursos = new List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>>();
             string consultaCategorias = "SELECT C.nombre AS nombreCurso,C.estado AS estado,C.precio AS precio," +
@@ -127,38 +130,41 @@ namespace PIBasesISGrupo1.Handler
                     byteArrayDocument = (byte[])columna["documento"],
                     tipoDocInformativo = Convert.ToString(columna["tipoDocumento"])
                 };
-                catalogo = new List <Tuple<string, string>>();
+                catalogo = new List<Tuple<string, string>>();
                 foreach (DataRow columnaTopicos in tableTopicos.Rows)
                 {
-                    if (String.Equals(cursoTemporal.nombre, Convert.ToString(columnaTopicos["nombreCurso"]))) {
-                        catalogo.Add(new Tuple <string, string>(Convert.ToString(columnaTopicos["topico"]), Convert.ToString(columnaTopicos["category"])));
+                    if (String.Equals(cursoTemporal.nombre, Convert.ToString(columnaTopicos["nombreCurso"])))
+                    {
+                        catalogo.Add(new Tuple<string, string>(Convert.ToString(columnaTopicos["topico"]), Convert.ToString(columnaTopicos["category"])));
                     }
 
                 }
-                    educadorTemporal = new Miembro
+                educadorTemporal = new Miembro
                 {
                     nombre = Convert.ToString(columna["nombreEducador"]),
                     primerApellido = Convert.ToString(columna["primerApellido"]),
                     segundoApellido = Convert.ToString(columna["segundoApellido"])
 
                 };
-               
-                cursos.Add(new Tuple<Cursos, Miembro, List<Tuple<string, string>>>(cursoTemporal, educadorTemporal,catalogo));
+
+                cursos.Add(new Tuple<Cursos, Miembro, List<Tuple<string, string>>>(cursoTemporal, educadorTemporal, catalogo));
 
 
             }
             return cursos;
         }
-       
-        public List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> buscarCursos(bool opcionDeBusqueda,string busqueda)
+
+        public List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> buscarCursos(bool opcionDeBusqueda, string busqueda)
         {
             List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> cursos = new List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>>();
             Tuple<string, string> comandosParaLaBusqueda;
-            if (opcionDeBusqueda == true) {
+            if (opcionDeBusqueda == true)
+            {
                 comandosParaLaBusqueda = crearConsultaParaBusquedaDeCurso(true, busqueda);
             }
-            else{
-                comandosParaLaBusqueda= crearConsultaParaBusquedaDeCurso(false, busqueda);
+            else
+            {
+                comandosParaLaBusqueda = crearConsultaParaBusquedaDeCurso(false, busqueda);
             }
             DataTable tablaCurso = crearTablaConsulta(comandosParaLaBusqueda.Item1);
             DataTable tableTopicos = crearTablaConsulta(comandosParaLaBusqueda.Item2);
@@ -246,7 +252,7 @@ namespace PIBasesISGrupo1.Handler
         }
         public bool aprobarCurso(string nombreCurso)
         {
-            
+
             string consulta = "UPDATE Curso " + "SET estado='Aprobado' " + "WHERE nombre=@nombreCurso";
             SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
             SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
@@ -256,28 +262,29 @@ namespace PIBasesISGrupo1.Handler
             conexion.Close();
             return exito;
         }
-        
-        public bool crearSeccion(SeccionModel seccion){
+
+        public bool crearSeccion(SeccionModel seccion)
+        {
             string consulta = "INSERT INTO Seccion(nombreSeccionPK,nombreCursoFK)"
             + "VALUES (@nombreSeccion,@nombreCurso)";
             SqlCommand comandoParaConsulta = baseDeDatos.crearComandoParaConsulta(consulta);
             comandoParaConsulta.Parameters.AddWithValue("@nombreCurso", seccion.nombreCurso);
             comandoParaConsulta.Parameters.AddWithValue("@nombreSeccion", seccion.nombreSeccion);
-            return baseDeDatos.ejecutarComandoParaConsulta(comandoParaConsulta); ;          
+            return baseDeDatos.ejecutarComandoParaConsulta(comandoParaConsulta); ;
         }
         public bool agregarMaterial(MaterialModel material, IFormFile archivo)
         {
-                string consulta = "INSERT INTO Material(nombreMaterialPK,nombreSeccionFK,nombreCursoFK, material, tipoArchivo)"
-                + "VALUES (@nombreMaterial,@nombreSeccion,@nombreCurso,@material,@tipoMaterial)";
-                SqlCommand comandoParaConsulta = baseDeDatos.crearComandoParaConsulta(consulta);
-                comandoParaConsulta.Parameters.AddWithValue("@nombreCurso", material.nombreDeCurso);
-                comandoParaConsulta.Parameters.AddWithValue("@nombreSeccion", material.nombreDeSeccion);
-                comandoParaConsulta.Parameters.AddWithValue("@nombreMaterial", material.nombreMaterial);
-                comandoParaConsulta.Parameters.AddWithValue("@material", obtenerBytes(archivo));
-                comandoParaConsulta.Parameters.AddWithValue("@tipoMaterial", archivo.ContentType);
+            string consulta = "INSERT INTO Material(nombreMaterialPK,nombreSeccionFK,nombreCursoFK, material, tipoArchivo)"
+            + "VALUES (@nombreMaterial,@nombreSeccion,@nombreCurso,@material,@tipoMaterial)";
+            SqlCommand comandoParaConsulta = baseDeDatos.crearComandoParaConsulta(consulta);
+            comandoParaConsulta.Parameters.AddWithValue("@nombreCurso", material.nombreDeCurso);
+            comandoParaConsulta.Parameters.AddWithValue("@nombreSeccion", material.nombreDeSeccion);
+            comandoParaConsulta.Parameters.AddWithValue("@nombreMaterial", material.nombreMaterial);
+            comandoParaConsulta.Parameters.AddWithValue("@material", obtenerBytes(archivo));
+            comandoParaConsulta.Parameters.AddWithValue("@tipoMaterial", archivo.ContentType);
 
             return baseDeDatos.ejecutarComandoParaConsulta(comandoParaConsulta);
-            
+
         }
 
         public List<SeccionModel> obtenerSecciones(string nombreCurso)
@@ -294,7 +301,7 @@ namespace PIBasesISGrupo1.Handler
                 {
                     nombreCurso = Convert.ToString(columna["nombreCursoFK"]),
                     nombreSeccion = Convert.ToString(columna["nombreSeccionPK"]),
-                    
+
                 });
 
             }
@@ -317,7 +324,7 @@ namespace PIBasesISGrupo1.Handler
                     archivo = (byte[])columna["material"],
                     nombreDeCurso = Convert.ToString(columna["nombreCursoFK"]),
                     nombreDeSeccion = Convert.ToString(columna["nombreSeccionFK"]),
-                    nombreMaterial =Convert.ToString(columna["nombreMaterialPK"]),
+                    nombreMaterial = Convert.ToString(columna["nombreMaterialPK"]),
                     tipoArchivo = Convert.ToString(columna["tipoArchivo"]),
                 });
 
@@ -325,7 +332,7 @@ namespace PIBasesISGrupo1.Handler
             return materiales;
         }
 
-        public bool borrarMaterial(MaterialModel material )
+        public bool borrarMaterial(MaterialModel material)
         {
             string consulta = "DELETE FROM Material " + "WHERE nombreCursoFK=@nombreCurso AND nombreSeccionFK=@nombreSeccion AND nombreMaterialPK=@nombreMaterial";
             SqlCommand comandoParaConsulta = baseDeDatos.crearComandoParaConsulta(consulta);
@@ -354,6 +361,7 @@ namespace PIBasesISGrupo1.Handler
             comandoParaConsulta.Parameters.AddWithValue("@nombreSeccionAntiguo", nombreSeccionAntiguo);
             return baseDeDatos.ejecutarComandoParaConsulta(comandoParaConsulta);
         }
+
         public bool crearCurso(string nombreCurso)
         {
 
@@ -370,14 +378,14 @@ namespace PIBasesISGrupo1.Handler
         public List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> obtenerCursosCreados()
         {
             List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> cursos = new List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>>();
-            string consultaCategorias = "SELECT C.nombre AS nombreCurso,C.estado AS estado,C.precio AS precio," +
+            string consultaCursos = "SELECT C.nombre AS nombreCurso,C.estado AS estado,C.precio AS precio," +
                 " C.emailEducadorFK AS emailEducador,C.tipoDocumentoInformativo AS tipoDocumento,C.documentoInformativo AS documento,E.nombre AS nombreEducador,E.primerApellido AS primerApellido,E.segundoApellido AS segundoApellido " +
             "FROM Curso C JOIN Usuario E ON C.emailEducadorFK = E.email " + "WHERE estado='Creado'";
             string consultaTopicos = "SELECT c.nombreCursoFK AS nombreCurso,T.nombreTopicoPK AS topico,Cat.nombreCategoriaPK AS category" +
             " FROM Curso Cu Join  Contiene C ON Cu.nombre = C.nombreCursoFK" +
             " JOIN Topico T ON C.nombreTopicoFK = T.nombreTopicoPK JOIN Categoria Cat ON Cat.nombreCategoriaPK = T.nombreCategoriaFK" +
             " WHERE Cu.estado = 'Creado'; ";
-            DataTable tablaCurso = crearTablaConsulta(consultaCategorias);
+            DataTable tablaCurso = crearTablaConsulta(consultaCursos);
             DataTable tableTopicos = crearTablaConsulta(consultaTopicos);
             Cursos cursoTemporal;
             Miembro educadorTemporal;
@@ -416,5 +424,59 @@ namespace PIBasesISGrupo1.Handler
             }
             return cursos;
         }
-    } 
+
+        public Tuple<Cursos, Miembro, List<Tuple<string, string>>> obtenerInformacionCurso(string nombreCurso)
+        {
+            Cursos curso = new Cursos();
+            Miembro educador = new Miembro();
+            List<Tuple<string, string>> catalogo;
+            string consultaCursos = "SELECT C.nombre AS nombreCurso,C.estado AS estado,C.precio AS precio," +
+                " C.emailEducadorFK AS emailEducador,C.tipoDocumentoInformativo AS tipoDocumento,C.documentoInformativo AS documento,E.nombre AS nombreEducador,E.primerApellido AS primerApellido,E.segundoApellido AS segundoApellido " +
+            "FROM Curso C JOIN Usuario E ON C.emailEducadorFK = E.email " + "WHERE estado='Creado' AND C.nombre = @nombreCurso";
+            string consultaTopicos = "SELECT c.nombreCursoFK AS nombreCurso,T.nombreTopicoPK AS topico,Cat.nombreCategoriaPK AS category" +
+            " FROM Curso Cu Join  Contiene C ON Cu.nombre = C.nombreCursoFK" +
+            " JOIN Topico T ON C.nombreTopicoFK = T.nombreTopicoPK JOIN Categoria Cat ON Cat.nombreCategoriaPK = T.nombreCategoriaFK" +
+            " WHERE Cu.estado = 'Creado' AND Cu.nombre = @nombreCurso; ";
+
+            SqlCommand comandoParaConsultaCursos = new SqlCommand(consultaCursos, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsultaCursos);
+            comandoParaConsultaCursos.Parameters.AddWithValue("@nombreCurso", nombreCurso);
+
+            conexion.Open();
+            SqlDataReader lectorDeDatosCurso = comandoParaConsultaCursos.ExecuteReader();
+            lectorDeDatosCurso.Read();
+            curso.nombre = Convert.ToString(lectorDeDatosCurso["nombreCurso"]);
+            curso.estado = Convert.ToString(lectorDeDatosCurso["estado"]);
+            curso.precio = Convert.ToDouble(lectorDeDatosCurso["precio"]);
+            curso.emailDelEducador = Convert.ToString(lectorDeDatosCurso["emailEducador"]);
+            curso.byteArrayDocument = (byte[])lectorDeDatosCurso["documento"];
+            curso.tipoDocInformativo = Convert.ToString(lectorDeDatosCurso["tipoDocumento"]);
+            conexion.Close();
+
+            conexion.Open();
+            SqlDataReader lectorDeDatosEducador = comandoParaConsultaCursos.ExecuteReader();
+            lectorDeDatosEducador.Read();
+            educador.nombre = Convert.ToString(lectorDeDatosEducador["nombreEducador"]);
+            educador.primerApellido = Convert.ToString(lectorDeDatosEducador["primerApellido"]);
+            educador.segundoApellido = Convert.ToString(lectorDeDatosEducador["segundoApellido"]);   
+            conexion.Close();
+
+            SqlCommand comandoParaConsultaTopicos = baseDeDatos.crearComandoParaConsulta(consultaTopicos);
+            comandoParaConsultaTopicos.Parameters.AddWithValue("@nombreCurso", nombreCurso);
+            DataTable tableTopicos = baseDeDatos.crearTablaConsulta(comandoParaConsultaTopicos);         
+            catalogo = new List<Tuple<string, string>>();
+            foreach (DataRow columnaTopicos in tableTopicos.Rows)
+            {
+                if (String.Equals(curso.nombre, Convert.ToString(columnaTopicos["nombreCurso"])))
+                {
+                    catalogo.Add(new Tuple<string, string>(Convert.ToString(columnaTopicos["topico"]), Convert.ToString(columnaTopicos["category"])));
+                }
+
+            }
+
+            Tuple<Cursos, Miembro, List<Tuple<string, string>>> cursos = new Tuple<Cursos, Miembro, List<Tuple<string, string>>>(curso, educador, catalogo);
+
+            return cursos;
+        }
+    }
 }
