2492cbd0a0cc9491780d6084cb3c6d439ba158ae
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-10-25 16:07:27 -0600

    Arreglo en la vista de cursos creados

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/CursoHandler.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/CursoHandler.cs
@@ -406,53 +406,14 @@ namespace PIBasesISGrupo1.Handler
             return exito;
         }
 
-        public List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> obtenerCursosCreados()
+        public List<string> obtenerCursosCreados(string emailEducador)
         {
-            List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>> cursos = new List<Tuple<Cursos, Miembro, List<Tuple<string, string>>>>();
-            string consultaCursos = "SELECT C.nombrePK AS nombreCurso,C.estado AS estado,C.precio AS precio," +
-                " C.emailEducadorFK AS emailEducador,C.tipoDocumentoInformativo AS tipoDocumento,C.documentoInformativo AS documento,E.nombre AS nombreEducador,E.primerApellido AS primerApellido,E.segundoApellido AS segundoApellido " +
-            "FROM Curso C JOIN Usuario E ON C.emailEducadorFK = E.emailPK " + "WHERE estado='Creado'";
-            string consultaTopicos = "SELECT c.nombreCursoFK AS nombreCurso,T.nombreTopicoPK AS topico,Cat.nombreCategoriaPK AS category" +
-            " FROM Curso Cu Join  Contiene C ON Cu.nombrePK = C.nombreCursoFK" +
-            " JOIN Topico T ON C.nombreTopicoFK = T.nombreTopicoPK JOIN Categoria Cat ON Cat.nombreCategoriaPK = T.nombreCategoriaFK" +
-            " WHERE Cu.estado = 'Creado'; ";
-            DataTable tablaCurso = crearTablaConsulta(consultaCursos);
-            DataTable tableTopicos = crearTablaConsulta(consultaTopicos);
-            Cursos cursoTemporal;
-            Miembro educadorTemporal;
-            List<Tuple<string, string>> catalogo;
-            foreach (DataRow columna in tablaCurso.Rows)
-            {
-                cursoTemporal = new Cursos
-                {
-                    nombre = Convert.ToString(columna["nombreCurso"]),
-                    estado = Convert.ToString(columna["estado"]),
-                    precio = Convert.ToDouble(columna["precio"]),
-                    emailDelEducador = Convert.ToString(columna["emailEducador"]),
-                    byteArrayDocument = (byte[])columna["documento"],
-                    tipoDocInformativo = Convert.ToString(columna["tipoDocumento"])
-                };
-                catalogo = new List<Tuple<string, string>>();
-                foreach (DataRow columnaTopicos in tableTopicos.Rows)
-                {
-                    if (String.Equals(cursoTemporal.nombre, Convert.ToString(columnaTopicos["nombreCurso"])))
-                    {
-                        catalogo.Add(new Tuple<string, string>(Convert.ToString(columnaTopicos["topico"]), Convert.ToString(columnaTopicos["category"])));
-                    }
+            List<string> cursos = new List<string>();
+            string consulta = "SELECT nombrePK FROM Curso WHERE emailEducadorFK=@emailEducador AND estado='Creado';";
 
-                }
-                educadorTemporal = new Miembro
-                {
-                    nombre = Convert.ToString(columna["nombreEducador"]),
-                    primerApellido = Convert.ToString(columna["primerApellido"]),
-                    segundoApellido = Convert.ToString(columna["segundoApellido"])
-
-                };
-
-                cursos.Add(new Tuple<Cursos, Miembro, List<Tuple<string, string>>>(cursoTemporal, educadorTemporal, catalogo));
-
-
-            }
+            SqlCommand comandoParaConsulta = baseDeDatos.crearComandoParaConsulta(consulta);
+            comandoParaConsulta.Parameters.AddWithValue("@emailEducador", emailEducador);
+            cursos = baseDeDatos.obtenerDatosDeColumna(comandoParaConsulta, "nombrePK");
             return cursos;
         }
 
