f886939a75ad5b9791761361a1be00d5af5f49ab
hellenfdz12 <hellenfdz12@gmail.com>
2020-11-07 12:12:05 -0600

    Nueva rama para el motor de similitudes para el perfil que estoy viendo.

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/SimilitudPerfilHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using PIBasesISGrupo1.Handler;
+
+namespace PIBasesISGrupo1.Handler
+{
+    public class SimilitudPerfilHandler
+    {
+
+
+        public SimilitudPerfilHandler()
+        {
+            conexionBD = new ConexionModel();
+            conexion = conexionBD.Connection();
+            baseDeDatos = new BaseDeDatosHandler();
+            miembrosHandler = new MiembroHandler();
+        }
+    }
+}
---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/MotorSimilitudes/MotorDeSimilitudes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using PIBasesISGrupo1.Handler;
+namespace PIBasesISGrupo1.MotorSimilitudes
+{
+    public class MotorDeSimilitudes
+    {
+       
+
+    }
+}
