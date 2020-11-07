02d4d506bec576524ebdac93f62d36b97fc3b4e3
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-07 11:50:37 -0600

    Se agregan variables necesarias al modelo

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/Certificado.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/Certificado.cs
@@ -7,5 +7,10 @@ namespace PIBasesISGrupo1.Models
 {
     public class Certificado
     {
+        public string nombreCurso { get; set; }
+        public string nombreEducador { get; set; }
+        public string nombreEstudiante { get; set; }
+        public string firmaEducador { get; set; }
+        public string firmaCoordinador { get; set; }
     }
 }
