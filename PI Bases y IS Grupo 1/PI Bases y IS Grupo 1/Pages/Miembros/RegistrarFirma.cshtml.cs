c72697f6238f26b739213dc4705a9d3bdd718240
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-08 11:20:06 -0600

    Se inicia a trabajar el login del miembvro

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembros/RegistrarFirma.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Miembros/RegistrarFirma.cshtml.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Mime;
+using System.IO;
+using PIBasesISGrupo1.Handler;
+using PIBasesISGrupo1.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Filters;
 
 namespace PIBasesISGrupo1.Pages.Miembros
 {
+    [PermisosDeVista("Coordinador")]
     public class RegistrarFirmaModel : PageModel
     {
         public void OnGet()
