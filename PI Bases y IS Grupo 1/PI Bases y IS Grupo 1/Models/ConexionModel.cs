06127f58dc42c19cf4a86ab6c6910f13332833dd
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-11-08 14:20:23 -0600

    Se ha modifica la comprobacion de los permisos de vista

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/ConexionModel.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/ConexionModel.cs
@@ -14,9 +14,9 @@ namespace PIBasesISGrupo1.Models
         public SqlConnection Connection() {
 
             SqlConnection con;
-            string conString = @"Server=grupo1.database.windows.net;Database=Grupo1;User ID=Grupo1;password=Adriancito1;";
-            //string conString = @"Server=172.16.202.75;Database=BD_Grupo1;User ID=Grupo1;password=Hellencita;";
-            //string conString = @"Server=LAPTOP-GAO7BEGA;Database=Demostracion_Comunidad;Integrated Security = True;Pooling = False";
+            //string conString = @"Server=grupo1.database.windows.net;Database=Grupo1;User ID=Grupo1;password=Adriancito1;";
+            string conString = @"Server=172.16.202.75;Database=BD_Grupo1;User ID=Grupo1;password=Hellencita;";
+            //string conString = @"Server=DESKTOP-ODOR35T\BASESEDEDATOS;Database=BD_Grupo1;Integrated Security = True;Pooling = False";
 
             con = new SqlConnection(conString);
             
