f7a8dfd035eb65889df42231e4cfc97e4b6c6520
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-08 11:59:36 -0600

    Se cambian datos del controlador

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/CrearCertificado.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/CrearCertificado.cshtml.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Mime;
+using System.IO;
+using PIBasesISGrupo1.Handler;
+using PIBasesISGrupo1.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Filters;
 
 namespace PIBasesISGrupo1.Pages.Curso
 {
