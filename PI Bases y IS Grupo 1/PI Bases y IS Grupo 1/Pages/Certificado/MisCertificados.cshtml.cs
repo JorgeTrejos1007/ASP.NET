cf5620dc5f298cb3c6fc3ff750886d6d0a8cb12e
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-08 11:45:34 -0600

    Se agregan las vistas para los certificados

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Certificado/MisCertificados.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIBasesISGrupo1.Pages.Certificado
+{
+    public class MisCertificadosModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
