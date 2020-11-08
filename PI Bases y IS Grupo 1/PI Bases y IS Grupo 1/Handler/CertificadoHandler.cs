e204e98d845f097f5775cf7331520fd1146a418f
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-08 15:04:29 -0600

    Se agrgan las vistas en el layout de certificados Se crea un handler de Certificados, el cual trae todos los certificados no aprobados

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Handler/CertificadoHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using PIBasesISGrupo1.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace PIBasesISGrupo1.Handler
+{
+    public class CertificadoHandler
+    {
+        private BaseDeDatosHandler baseDeDatos;
+        public CertificadoHandler()
+        {
+           baseDeDatos = new BaseDeDatosHandler();
+        }
+        public List<Certificado> obtenerCertificadosNoAprobados(){
+            List<Certificado> listaDeCertificados = new List<Certificado>();
+            string consulta = "SELECT E.nombre AS 'nombreEducador'  ,Cu.nombrePK AS 'nombreCurso', S.nombre AS 'nombreEstudiante' " +
+                "FROM Certifica Certi " +
+                "JOIN Usuario E ON E.emailPK = Certi.emailEducadorFK " +
+                "JOIN Curso Cu ON Cu.nombrePK = Certi.nombreCursoFK " +
+                "JOIN Certificado C ON C.idCertificadoPK = Certi.idCertificadoFK " +
+                "JOIN Usuario S ON Certi.emailEstudianteFK = S.emailPK " +
+                "WHERE c.estadoDelCertificado = 'No Aprobado'; ";
+            SqlCommand comando= baseDeDatos.crearComandoParaConsulta(consulta);
+            DataTable tablaCurso = baseDeDatos.crearTablaConsulta(comando);
+            Certificado certificadoTemporal;
+            foreach (DataRow columna in tablaCurso.Rows)
+            {
+                certificadoTemporal = new Certificado
+                {
+                    nombreCurso = Convert.ToString(columna["nombreCurso"]),
+                    nombreEducador = Convert.ToString(columna["emailEducador"]),
+                    nombreEstudiante = Convert.ToString(columna["emailEstudiante"]),
+
+                };
+                listaDeCertificados.Add(certificadoTemporal);
+            }
+            return listaDeCertificados;
+        }
+
+    }
+}
