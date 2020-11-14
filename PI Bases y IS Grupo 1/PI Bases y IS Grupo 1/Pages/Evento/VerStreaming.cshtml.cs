7a637a9ab44a9ad8b5db8aca4541753a77deee4a
AdrianRojas <rojas3099@gmail.com>
2020-11-13 20:32:09 -0600

    nueva rama para probar el stream

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Evento/VerStreaming.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIBasesISGrupo1.Pages.Evento
+{
+    public class VerStreamingModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
