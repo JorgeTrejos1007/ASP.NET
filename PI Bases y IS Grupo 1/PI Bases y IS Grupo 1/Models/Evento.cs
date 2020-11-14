f45900730ee4d735728a0e8e08853f41177f340f
hellenfdz12 <hellenfdz12@gmail.com>
2020-11-14 13:09:59 -0600

    Listos los modelos de evento y sector del evento presencial.

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/Evento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //Para las librerias de Required y Display. Para hacer binding de las propiedades del modelo vista
+
+namespace PIBasesISGrupo1.Models
+{
+    public class Evento
+    {
+        [Required(ErrorMessage = "Es necesario que ingreses el nombre del evento")]
+        [Display(Name = "Ingrese el Nombre del evento")]
+        [RegularExpression(@"^[a-zA-Z\s][a-zA-Z-0-9\s]+$", ErrorMessage = "Por favor ingrese solo letras y numeros")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingreses la fecha del evento")]
+        [Display(Name = "Ingrese la fecha del evento")]
+        public DateTime fechaYHora { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingreses la información sobre el evento")]
+        [Display(Name = "Ingrese informacion del evento")]
+        public string descripcionDelEvento{ get; set; }
+
+        public string tipoArchivoImagen { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingreses una imagen para el evento")]
+        [Display(Name = "Ingrese la imagen de la noticia")]
+        public byte[] arrayArchivoImagen { get; set; }
+      
+        [Display(Name = "Ingrese el nombre del canal de Twitch donde se va a realizar la transimision del evento")]
+        [RegularExpression(@"^[a-zA-Z\s][a-zA-Z-0-9\s]+$", ErrorMessage = "Por favor ingrese solo letras y numeros")]
+        public string nombreCanalStream { get; set; }
+      
+        [Display(Name = "Ingrese ingreses la direccion de dónde se va a llevar a cabo el evento")]
+        public string lugar { get; set; }
+
+        public List<Sector> sectores { get; set; }
+    }
+}
---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Models/Sector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; 
+
+namespace PIBasesISGrupo1.Models
+{
+    public class Sector
+    {
+        [Required(ErrorMessage = "Es necesario que ingrese el nombre del sector")]
+        [Display(Name = "Ingrese el nombre del sector")]
+        [RegularExpression(@"^[a-zA-Z\s][a-zA-Z-0-9\s]+$", ErrorMessage = "Por favor ingrese el nombre del sector")]
+        public string nombreDeSector { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingrese el tipo de sector")]
+        [Display(Name = "Ingrese el tipo")]
+        [RegularExpression(@"^(Numerado|No numerado)$", ErrorMessage = "Por favor ingrese si el sector es Numerado o No numerado")]
+        public string tipo { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingrese la cantidad de asientos")]
+        [Display(Name = "Ingrese la cantidad de asientos")]
+        [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "Por favor ingrese solo numeros")]
+        public int cantidadAsientos { get; set; }
+    }
+}
