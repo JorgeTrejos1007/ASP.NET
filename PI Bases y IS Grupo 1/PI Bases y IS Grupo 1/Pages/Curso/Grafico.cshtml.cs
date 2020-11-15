f4ed9a7f6668b429edc1c8c88c1ffadf1c75da19
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-15 12:30:09 -0600

    Se agrega la vista, modelo y controlador grafico

---
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Curso/Grafico.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using PIBasesISGrupo1.Models;
+namespace PIBasesISGrupo1.Pages.Curso
+{
+    public class GraficoModel : PageModel
+    {
+        // GET: Home
+        public void OnGet()
+        {
+            List<DataPoint> dataPoints = new List<DataPoint>();
+
+            dataPoints.Add(new DataPoint("NXP", 14));
+            dataPoints.Add(new DataPoint("Infineon", 10));
+            dataPoints.Add(new DataPoint("Renesas", 9));
+            dataPoints.Add(new DataPoint("STMicroelectronics", 8));
+            dataPoints.Add(new DataPoint("Texas Instruments", 7));
+            dataPoints.Add(new DataPoint("Bosch", 5));
+            dataPoints.Add(new DataPoint("ON Semiconductor", 4));
+            dataPoints.Add(new DataPoint("Toshiba", 3));
+            dataPoints.Add(new DataPoint("Micron", 3));
+            dataPoints.Add(new DataPoint("Osram", 2));
+            dataPoints.Add(new DataPoint("Others", 35));
+
+            ViewData["Grafico"] = JsonConvert.SerializeObject(dataPoints);
+
+           
+        }
+    }
+}
