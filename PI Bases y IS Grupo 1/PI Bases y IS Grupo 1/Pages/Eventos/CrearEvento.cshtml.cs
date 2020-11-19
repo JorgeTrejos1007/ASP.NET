2056c43682a2fee6502f7da2d10f8d0967123ffd
Carlos Cartín Cascante <ala53@hotmail.com>
2020-11-19 15:19:50 -0600

    Cambios a crear evento

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/CrearEvento.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/CrearEvento.cshtml.cs
@@ -16,6 +16,7 @@ namespace PIBasesISGrupo1.Pages.Eventos
         public Evento evento { get; set; }
 
         [BindProperty]
+        [RegularExpression(@"([a-zA-Z0-9\s_\\.\-:])+(.png|.jpg)$", ErrorMessage = "Ingrese una imagen png o jpg")]
         [Required(ErrorMessage = "Es necesario que ingreses una imagen del evento")]
         public IFormFile imagenEvento { get; set; }
 
@@ -31,39 +32,64 @@ namespace PIBasesISGrupo1.Pages.Eventos
 
         public void OnGet()
         {
-
+            if (TempData["MensajeError"] == null)
+            {
+                TempData["MensajeError"] = "";
+            }
         }
 
         public IActionResult OnPostCrearEventoVirtual()
         {
             IActionResult vista;
-            try {
+            try
+            {
                 Miembro datosDelMiembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, User.Identity.Name);
                 evento.emailCoordinador = datosDelMiembro.email;
                 evento.fechaYHora = Convert.ToDateTime(concatenarFechaYHora(fecha, hora));
                 EventoHandler accesoDatos = new EventoHandler();
-                accesoDatos.registrarEvento(evento, imagenEvento);
-                accesoDatos.registrarEventoVirtual(evento);
-                vista = Redirect("~/Eventos/MostrarEventos");
+                if (accesoDatos.registrarEvento(evento, imagenEvento))
+                {
+                    if (accesoDatos.registrarEventoVirtual(evento))
+                    {
+                        vista = Redirect("~/Eventos/MostrarEventos");
+                    }
+                    else
+                    {
+                        vista = Redirect("~/Eventos/CrearEvento");
+                    }
+                }
+                else
+                {
+                    vista = Redirect("~/Eventos/CrearEvento");
+                    TempData["MensajeError"] = "Este evento ya fue creado anteriormente";
+                }
+
+
+
             }
             catch
             {
                 vista = Redirect("~/Error");
             }
             return vista;
-            
+
         }
+
 
         public IActionResult OnPostCrearSeccionEventoPresencial()
         {
             IActionResult vista;
             try
             {
-
-                Miembro datosDelMiembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, User.Identity.Name); 
-                TempData["emailCoordinador"] = datosDelMiembro.email;
+                Miembro datosDelMiembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, User.Identity.Name);
+                evento.emailCoordinador = datosDelMiembro.email;
+                evento.fechaYHora = Convert.ToDateTime(concatenarFechaYHora(fecha, hora));
                 TempData["nombreEvento"] = evento.nombre;
-                TempData["fechaYHora"] = Convert.ToDateTime(concatenarFechaYHora(fecha, hora));
+                TempData["fechaYHora"] = evento.fechaYHora;
+                EventoHandler accesoDatos = new EventoHandler();
+                accesoDatos.registrarEvento(evento, imagenEvento);
+                accesoDatos.registrarEventoPresencial(evento);
+
                 vista = Redirect("~/Eventos/CrearSectores");
             }
             catch
@@ -74,8 +100,29 @@ namespace PIBasesISGrupo1.Pages.Eventos
         }
 
         public string concatenarFechaYHora(string fecha, string hora)
-        {     
-            return fecha+" "+hora;
+        {
+            return fecha + " " + hora;
+        }
+
+        public bool tipoImagenValido(IFormFile imagen)
+        {
+            bool tipoValido = false;
+            if ((imagen != null) && ((imagen.ContentType == "image/jpeg" || imagen.ContentType == "image/png")))
+            {
+                tipoValido = true;
+            }
+            return tipoValido;
+        }
+
+        public bool fechaYHoraValida(DateTime fechaYHora)
+        {
+            bool fechaValida = false;
+            if (fechaYHora >= DateTime.UtcNow)
+            {
+                fechaValida = true;
+            }
+            return fechaValida;
+
         }
     }
 }
--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/CrearSectores.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/CrearSectores.cshtml.cs
@@ -11,20 +11,65 @@ namespace PIBasesISGrupo1.Pages.Eventos
 {
     public class CrearSectoresModel : PageModel
     {
+        [BindProperty]
+        public Sector sector { get; set; }
+
         public IActionResult OnGet()
         {
             IActionResult vista;
             try
             {
-                ViewData["nombreEvento"] = TempData["nombreEvento"];
-                TempData["nombreEvento"] = ViewData["nombreEvento"];
-                vista = Page();
+                if (TempData["nombreEvento"] == null)
+                {
+                    vista = Redirect("~/Error");
+                }
+                else
+                {
+                    ViewData["nombreEvento"] = TempData["nombreEvento"];
+                    TempData["nombreEvento"] = ViewData["nombreEvento"];
+                    vista = Page();
+                }
+
             }
             catch
             {
                 vista = Redirect("~/Error");
             }
             return vista;
+        }
+
+        public IActionResult OnPostAgregarSector(string nombre, int cantidadDeAsientos, string tipoAsiento)
+        {
+            if (nombre == null || cantidadDeAsientos == 0 || tipoAsiento == null)
+            {
+                return new JsonResult("FaltanDatos");
+            }
+            else
+            {
+                EventoHandler accesoDatos = new EventoHandler();
+                Miembro datosDelMiembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, User.Identity.Name);
+                sector.nombreDeSector = nombre;
+                sector.cantidadAsientos = cantidadDeAsientos;
+                sector.tipo = tipoAsiento;
+                string nombreEvento = (string)TempData["nombreEvento"];
+                DateTime fechaYHora = (DateTime)TempData["fechaYHora"];
+                TempData["fechaYHora"] = fechaYHora;
+                TempData["nombreEvento"] = nombreEvento;
+                if (accesoDatos.registrarSector(sector, datosDelMiembro.email, nombreEvento, fechaYHora))
+                {
+                    return new JsonResult(sector);
+                }
+                else
+                {
+                    return new JsonResult("ErrorAlAñadirSector");
+                }
+
+            }
+        }
+
+        public IActionResult OnPostTerminarCrearSectores()
+        {
+            return Redirect("~/Eventos/MostrarEventos");
         }
     }
 }
