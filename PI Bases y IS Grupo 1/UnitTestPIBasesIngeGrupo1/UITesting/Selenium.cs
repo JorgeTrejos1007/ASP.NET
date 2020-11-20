c64c8d6f6cefa8a03d7dd735ad9762b8c1032eb2
AdrianRojas <rojas3099@gmail.com>
2020-11-20 10:58:09 -0600

    Rama para pruebas Selenium

---
+++ PI Bases y IS Grupo 1/UnitTestPIBasesIngeGrupo1/UITesting/Selenium.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace UnitTestPIBasesIngeGrupo1.UITesting
+{
+    [TestClass]
+    public class Selenium
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ///Arrange
+            ///Crea el driver de Chrome
+            driver = new ChromeDriver();
+            string URL = "https://localhost:44326/";
+            ///Pone la pantalla en full screen
+            driver.Manage().Window.Maximize();
+            ///Act
+            //////Se va a la URL indicada
+            driver.Url = URL;
+            ///Assert
+
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
