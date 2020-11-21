2eef54e14f972d6b224286b94daba85b5f8e2d63
Ronny <ronny.castroesquivel@ucr.ac.cr>
2020-11-21 15:11:42 -0600

    Se ha iniciado la prueba selenium

--- PI Bases y IS Grupo 1/UnitTestPIBasesIngeGrupo1/UITesting/Selenium.cs
+++ PI Bases y IS Grupo 1/UnitTestPIBasesIngeGrupo1/UITesting/Selenium.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
 
 namespace UnitTestPIBasesIngeGrupo1.UITesting
@@ -16,12 +17,34 @@ namespace UnitTestPIBasesIngeGrupo1.UITesting
             ///Arrange
             ///Crea el driver de Chrome
             driver = new ChromeDriver(Environment.CurrentDirectory);
-            string URL = "https://localhost:44326/";
+            string URL = "https://localhost:44326/Login/LoginMiembro";
             ///Pone la pantalla en full screen
             driver.Manage().Window.Maximize();
             ///Act
             //////Se va a la URL indicada
             driver.Url = URL;
+          
+            IWebElement campoCorreo = driver.FindElement(By.Id("correo"));
+            IWebElement campoPass = driver.FindElement(By.Id("pass"));
+ 
+            campoCorreo.SendKeys("ronnyale0@hotmail.com");
+            campoPass.SendKeys("Esquivel");
+            IWebElement form = driver.FindElement(By.Id("myForm"));
+            form.Submit();
+            IWebElement cursos = driver.FindElement(By.Id("cursos"));
+            cursos.Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(ExpectedConditions.ElementToBeClickable(By.Id("cursosInscritos")));
+
+
+
+            driver.FindElement(By.Id("cursosInscritos")).Click();
+
+
+            /*WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement cursosInscritos=wait.Until(e=>e.FindElement(By.Id("cursosInscritos")));
+            cursosInscritos.Click();*/
+            //IWebElement cursosInscritos = driver.FindElement(By.Id("cursosInscritos"));
+            //cursosInscritos.Click();
             ///Assert
 
         }
