f2e93aa9e3e5960b4ae1c88955e1051abc54474e
Stideen <Stideen.garbanzo@ucr.ac.cr>
2020-11-22 23:57:26 -0600

    Se agregan 2 nuevas pruebas unitarias

--- PI Bases y IS Grupo 1/UnitTestPIBasesIngeGrupo1/UITesting/Selenium.cs
+++ PI Bases y IS Grupo 1/UnitTestPIBasesIngeGrupo1/UITesting/Selenium.cs
@@ -48,6 +48,64 @@ namespace UnitTestPIBasesIngeGrupo1.UITesting
 
 
         }
+        [TestMethod]
+        public void pruebaDeNumeroDeIdiomasEnLaComunidad()
+        {
+            ///Arrange
+            ///Crea el driver de Chrome
+            driver = new ChromeDriver(Environment.CurrentDirectory);
+            string URL = "https://localhost:44326/Login/LoginMiembro";
+            ///Pone la pantalla en full screen
+            driver.Manage().Window.Maximize();
+            ///Act
+            //////Se va a la URL indicada
+            driver.Url = URL;
+
+            IWebElement campoCorreo = driver.FindElement(By.Id("correo"));
+            IWebElement campoPass = driver.FindElement(By.Id("pass"));
+
+            campoCorreo.SendKeys("stevegc112016@gmail.com");
+            campoPass.SendKeys("123456");
+            IWebElement form = driver.FindElement(By.Id("myForm"));
+            form.Submit();
+            IWebElement cursos = driver.FindElement(By.Id("analitica"));
+            cursos.Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(ExpectedConditions.ElementToBeClickable(By.Id("analiticaComunidad")));
+            driver.FindElement(By.Id("analiticaComunidad")).Click();
+            IWebElement idiomas = driver.FindElement(By.Id("totalIdiomas"));
+            Assert.AreEqual("12", idiomas.Text);
+        }
+        [TestMethod]
+        public void pruebaVerificarCertificadoDisponibleParaAprobar()
+        {
+            ///Arrange
+            ///Crea el driver de Chrome
+            driver = new ChromeDriver(Environment.CurrentDirectory);
+            string URL = "https://localhost:44326/Login/LoginMiembro";
+            ///Pone la pantalla en full screen
+            driver.Manage().Window.Maximize();
+            ///Act
+            //////Se va a la URL indicada
+            driver.Url = URL;
+
+            IWebElement campoCorreo = driver.FindElement(By.Id("correo"));
+            IWebElement campoPass = driver.FindElement(By.Id("pass"));
+
+            campoCorreo.SendKeys("stevegc112016@gmail.com");
+            campoPass.SendKeys("123456");
+            IWebElement form = driver.FindElement(By.Id("myForm"));
+            form.Submit();
+            IWebElement certificados = driver.FindElement(By.Id("certificados"));
+            certificados.Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(ExpectedConditions.ElementToBeClickable(By.Id("aprobar")));
+            driver.FindElement(By.Id("aprobar")).Click();
+            IWebElement estudiante = driver.FindElement(By.Id("estudiante"));
+            Assert.AreEqual("Juan Pablo Gutierrez", estudiante.Text);
+            IWebElement curso = driver.FindElement(By.Id("curso"));
+            Assert.AreNotEqual("Algebra Lineal 1", curso.Text);
+            IWebElement educador = driver.FindElement(By.Id("educador"));
+            Assert.AreEqual("Ronny Castro Esquivel", educador.Text);
+         }
 
 
 
