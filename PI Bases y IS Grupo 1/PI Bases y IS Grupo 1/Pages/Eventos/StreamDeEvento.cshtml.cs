9aada773353d57490e630404bd9cda85dbcb0cd5
Jorge <jorge.trejosbarquero@ecci.ucr.ac.cr>
2020-11-23 10:50:09 -0600

    Se creo vista controlador para ver eventos en stream, abierto a modificaciones.

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/StreamDeEvento.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/StreamDeEvento.cshtml.cs
@@ -9,9 +9,12 @@ namespace PIBasesISGrupo1.Pages.Eventos
 {
     public class StreamDeEventoModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string nombreE, string fecha, string email)
         {
-
+            nombreE = "Gamplay con auron"; 
+            TempData["nombreEvento"] = nombreE;
+            /*Aqui se llama al handler para conseguir el nombre del canal con los datos dados en parametros*/
+            TempData["nombreCanal"] = "auronplay";
         }
     }
 }
