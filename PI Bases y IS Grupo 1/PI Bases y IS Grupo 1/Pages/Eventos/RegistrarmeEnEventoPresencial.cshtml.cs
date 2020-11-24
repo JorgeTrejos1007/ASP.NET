258fa84612fb1fe3512af54ff9f5c0db723c22bd
Jorge <jorge.trejosbarquero@ecci.ucr.ac.cr>
2020-11-23 18:09:29 -0600

    Envia correo cuando se registra en eventos presenciales

--- PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/RegistrarmeEnEventoPresencial.cshtml.cs
+++ PI Bases y IS Grupo 1/PI Bases y IS Grupo 1/Pages/Eventos/RegistrarmeEnEventoPresencial.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PIBasesISGrupo1.Handler;
 using PIBasesISGrupo1.Models;
+using System.Net.Mail;
 
 namespace PIBasesISGrupo1.Pages.Eventos
 {
@@ -25,6 +26,7 @@ namespace PIBasesISGrupo1.Pages.Eventos
                 string nombreEvento = (string)TempData["nombreEvento"];
                 DateTime fechaYHora = (DateTime)TempData["fechaEvento"];
                 string lugar = (string)TempData["lugarEvento"];
+                TempData["nombreLugar"] = lugar;
                 ViewData["nombreEvento"] = nombreEvento;
                 ViewData["fechaYHora"] = fechaYHora;
                 ViewData["lugarEvento"] = lugar;
@@ -65,19 +67,58 @@ namespace PIBasesISGrupo1.Pages.Eventos
 
             vista = Redirect("~/index");
 
+            var miembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, "Miembro");
+            bool exito = true;
+
             if (registro.tipoDeSector == "Numerado") {
-                var miembro = Sesion.obtenerDatosDeSesion(HttpContext.Session, "Miembro");
-                bool exito = baseDeDatosHandler.transaccionReservarAsientosNumerados(registro, miembro.email);
+                exito = baseDeDatosHandler.transaccionReservarAsientosNumerados(registro, miembro.email);
                 if (exito == false) {
                     vista = Redirect("~/index");
                 }
             }
             else
             {
-                bool exito = baseDeDatosHandler.transaccionReservarAsientosNoNumerados(registro);
+                exito = baseDeDatosHandler.transaccionReservarAsientosNoNumerados(registro);
                 if (exito == false) {
                     vista = Redirect("~/index");
                 }
+            }
+
+            if (exito)
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("comunidad.practica.g1@gmail.com");
+                mail.To.Add(miembro.email);
+                mail.Subject = "Entradas para el evento " + registro.nombreEvento;
+
+
+                mail.Body = "Hola, usted ha sido registrado en un evento. Los detalles de se registro se muestran a continuación:\n\n";
+                mail.Body += "Evento: " + registro.nombreEvento + ".\n";
+                mail.Body += "Sector: " + registro.nombreSector + ".\n";
+
+                if (registro.tipoDeSector == "Numerado")
+                {
+                    mail.Body += "Asientos: ";
+                    for (int index = 0; index < registro.asientosDeseados.Count; index++)
+                    {
+                        mail.Body += registro.asientosDeseados[index].ToString() + " ";
+                    }
+                }
+                else
+                {
+                    mail.Body += "Cantidad de asientos: " + registro.cantidadAsientos.ToString();
+                }
+
+                mail.Body += ".\nFecha: " + registro.fechaYHora.ToString("dd/MM/yyyy") + ".\n";
+                mail.Body += "Hora: " + registro.fechaYHora.ToString("HH:mm");
+                mail.Body += "\nLugar: " + (string)TempData["nombreLugar"] + ".\n\n";
+                mail.Body += "Presente este correo en la entrada el día del evento para poder ingresar.\nTe esperamos!";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("comunidad.practica.g1@gmail.com", "AdriancitoG1.");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
             }
 
             return vista;
